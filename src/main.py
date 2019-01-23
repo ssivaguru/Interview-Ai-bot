42209703484652ebac4167aac2494ed6d9f00a46
sivaguru <sivaguru.t@zohocorp.com>
2019-01-23 22:35:54 +0530

    data to csv file

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import numpy as np
 
 IMAGE_DIR = "Images"
 
-record = re.recorder()
+#record = re.recorder()
 
 ###remove file if already present
 record.fileCheck()
@@ -24,9 +24,24 @@ for file in os.listdir(IMAGE_DIR):
 
 EAnalysis = load_model("../Emotion_analysis")
 
-img = cv2.imread("/home/local/ZOHOCORP/siva-5548/Documents/Artificial intelligence/Interview Ai/Interview-Ai-bot/src/Face/image15.jpg")
-lastimg = cv2.resize(img, (48, 48))
-gray_image = lastimg/255
-out = gray_image[np.newaxis, :, :]
-predictions = EAnalysis.predict(out)
-print(he.getEmotion(predictions))
+
+sad = np.array([["Image","Emotion"]], dtype=object)
+predit = np.array([[ 'Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise','Neutral']],dtype=object)
+for file in os.listdir(he.FACEDIR):
+    face = os.path.join(he.FACEDIR,file)
+    img = cv2.imread(face)
+    lastimg = cv2.resize(img, (48, 48))
+    gray_image = lastimg/255
+    out = gray_image[np.newaxis, :, :]
+    predictions = EAnalysis.predict(out)
+    emotion = he.getEmotion(predictions)
+    sad = np.append(sad,[[face,emotion]],axis=0)
+    predit = np.append(predit,predictions,axis=0)
+
+f=open('emotion.dat','ab')
+np.savetxt(f,sad,fmt='%s')
+f.close()
+
+x=open('prediction.dat','ab')
+np.savetxt(x,predit,fmt='%s')
+x.close()
--- src/recorder.py
+++ src/recorder.py
@@ -35,7 +35,7 @@ class recorder:
         cap = cv2.VideoCapture(0)
         # Define the codec and create VideoWriter object
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter(self.videoFilename,fourcc, 20.0, (640,480))
+        out = cv2.VideoWriter(self.videoFilename,fourcc, 30.0, (640,480))
         count = 0
         while(cap.isOpened()):
             ret, frame = cap.read()
